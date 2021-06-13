8890176faba83bdbfcf5bbba86c0a572a89951f6
WinterInside <de_animator@mail.ru>
2021-06-13 16:41:18 +0300

    add mobile version layout. add JQuery plugins to tabs and input

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,18 @@ menu.addEventListener('click', function() {
     nav.classList.add('navigation__list--closed');
   }
 });
+
+$(document).ready(function($) {
+  $('.tab_content').hide();
+  $('.tab_content:first').show();
+  $('.tabs li:first').addClass('active');
+  $('.tabs li').click(function(event) {
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide();
+
+    var selectTab = $(this).find('a').attr("href");
+
+    $(selectTab).fadeIn();
+  });
+});
